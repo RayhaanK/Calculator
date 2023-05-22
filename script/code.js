719a7c9c6025c1737ebd130b91619a4b2bae1ac1
RayhaanK <rayhaankhan331@gmail.com>
2023-05-22 12:35:37 +0200

    functionality

--- script/code.js
+++ script/code.js
@@ -1,10 +1,29 @@
-let firstOp = 0;
-let operator = "";
-let result = document.querySelector('#output');
+// Declaring Variables
+const clear = document.querySelector('#clear')
+const screen = document.querySelector('#output')
+const backspace = document.querySelector('#backspace')
+const equal = document.querySelector('#equal')
 
+// displays Numbers
 function display(value) {
-    result.value 
+    screen.value 
     += value;
 }
 
-result = display(value).innerText;
+// backspace button
+backspace.addEventListener('click', ()=>{
+    let output = screen.value;
+    screen.value = output.slice(0, -1);
+})
+
+// Equal Button
+equal.addEventListener('click', ()=> {
+    let output = eval(screen.value)
+    screen.value = output.toFixed(2);
+})
+
+// Clear Button
+clear.addEventListener('click', ()=> {
+    let output = "";
+    screen.value = output;
+})
