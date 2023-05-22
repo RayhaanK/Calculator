8833cf8b1b5f971ecacadc8ed8e0b8f04d1d5563
RayhaanK <rayhaankhan331@gmail.com>
2023-05-22 15:29:09 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,14 +1,72 @@
 // Declaring Variables
-const clear = document.querySelector('#clear')
+const clears = document.querySelector('#clear')
 const screen = document.querySelector('#output')
 const backspace = document.querySelector('#backspace')
 const equal = document.querySelector('#equal')
+const numbers = document.querySelector('.numbers')
+const decimal = document.querySelector('.decimal')
+let checkerArray = [];
 
 // displays Numbers
 function display(value) {
-    screen.value 
-    += value;
+    if(value == '.' && checkerArray.includes('.')){
+        return;
+    } else if(value == '+' || value == '-' || value == '*' || value == '/'){
+    } else {
+        screen.value += value;
+        checkerArray.push(value);
+    } 
 }
+
+// removing repetition of operators
+document.querySelector('#add')
+.addEventListener('click', (e)=>{
+    let inputArray = screen.value.split('');
+    console.log(inputArray)
+    if(inputArray[inputArray.length-1] === '+'){
+        return;
+    } else {
+        screen.value += e.target.innerHTML;
+        checkerArray = [];
+    }
+    })
+
+    document.querySelector('#subtract')
+    .addEventListener('click', (e)=>{
+        let inputArray = screen.value.split('');
+        console.log(inputArray)
+        if(inputArray[inputArray.length-1] === '-'){
+            return;
+        } else {
+            screen.value += e.target.innerHTML;
+            checkerArray = [];
+        }
+    })
+
+document.querySelector('#divide')
+.addEventListener('click', (e)=>{
+    let inputArray = screen.value.split('');
+    console.log(inputArray)
+    if(inputArray[inputArray.length-1] === '/'){
+        return;
+    } else {
+        screen.value += e.target.innerHTML;
+        checkerArray = [];
+    }
+    })
+
+    document.querySelector('#multiply')
+.addEventListener('click', (e)=>{
+    let inputArray = screen.value.split('');
+    console.log(inputArray)
+    if(inputArray[inputArray.length-1] === 'x'){
+        return;
+    } else {
+        screen.value += e.target.innerHTML;
+        checkerArray = [];
+    }
+})
+
 
 // backspace button
 backspace.addEventListener('click', ()=>{
@@ -22,8 +80,12 @@ equal.addEventListener('click', ()=> {
     screen.value = output.toFixed(2);
 })
 
+function clear() {
+    screen.value = "";
+}
+
 // Clear Button
-clear.addEventListener('click', ()=> {
-    let output = "";
-    screen.value = output;
+clears.addEventListener('click', ()=> {
+   clear(); 
 })
+
