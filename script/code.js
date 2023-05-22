3b665130acf7c4986e49f1b01cd76bd8a9aa6d5a
RayhaanK <rayhaankhan331@gmail.com>
2023-05-22 10:34:59 +0200

    html and css

---
+++ script/code.js
@@ -0,0 +1,10 @@
+let firstOp = 0;
+let operator = "";
+let result = document.querySelector('#output');
+
+function display(value) {
+    result.value 
+    += value;
+}
+
+result = display(value).innerText;
